3b22789b3a7615d7608046505af39b32ee23690b
Gerson <ondangangerson@gmail.com>
2022-12-15 22:08:00 +0800

    updates for template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,78 +3,64 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+   
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!--favicon-->
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png">
+    <!--bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!--owl carousel css-->
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <!--magnific popup css-->
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+    <!--icomoon css-->
+    <link rel="stylesheet" type="text/css" href="css/icomoon.css">
+    <!--icofont css-->
+    <link rel="stylesheet" type="text/css" href="css/icofont.min.css">
+    <!--animate css-->
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <!--main css-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!--responsive css-->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- Scripts -->   
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}  <!--font size won't work-->
+    @vite([ 'resources/js/app.js'])   <!--font size works-->
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+    <div id="app">  
+        <main >
             @yield('content')
         </main>
     </div>
+    
+    <!--end footer-->
+    <!--jQuery js-->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!--proper js-->
+    <script src="js/popper.min.js"></script>
+    <!--bootstrap js-->
+    <script src="js/bootstrap.min.js"></script>
+    <!--magnic popup js-->
+    <script src="js/magnific-popup.min.js"></script>
+    <!--owl carousel js-->
+    <script src="js/owl.carousel.min.js"></script>
+    <!--scrollIt js-->
+    <script src="js/scrollIt.min.js"></script>
+    <!--validator js-->
+    <script src="js/validator.min.js"></script>
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <!--main js-->
+    <script src="js/custom.js"></script>
 </body>
 </html>
